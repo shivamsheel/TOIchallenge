aa0f9f97b86cc6e876a1d8c51d0cef4bfd4b6e7b
shivamsheel <shivam_sheel@yahoo.co.in>
2020-12-12 12:15:20 +0530

    changes

--- controllers/verifyController.js
+++ controllers/verifyController.js
@@ -1,5 +1,5 @@
 let ACCOUNT_SID="AC592debfec8694fdca9503b300e6bb471"
-let AUTH_TOKEN="c3fc84cb96824f156a7f8db785dcc652" // changed
+let AUTH_TOKEN="6e4cfa65f6b7faf11c801a1056824a02" // changed
 let VERIFY_SERVICE_SID="VAe093d0523158de302a59d66a6a45ee8f"
 
 const client = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
